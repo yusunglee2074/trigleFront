f5fd1719ad03675e4822ec32ef7ee625df524ca5
lee yusung <yusunglee2074@lees-MBP.T-mobile.com>
2018-11-06 16:52:08 +0900

    import from app.js for navigation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import AuthScreen from './src/screens/Auth';
+import Navigation from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => AuthScreen);
+AppRegistry.registerComponent(appName, () => Navigation);
