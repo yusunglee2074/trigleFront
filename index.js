97344091fb1163c805554a65f7381af05cb4811f
lee yusung <yusunglee2074@lees-MBP.T-mobile.com>
2018-11-06 15:22:10 +0900

    move entry component, App.js to Auth.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import AuthScreen from './src/screens/Auth';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AuthScreen);
