00a240a987599c9a6ef3526dfac46e2b477632c1
lee yusung <yusunglee2074@lees-MBP.T-mobile.com>
2018-11-06 16:51:37 +0900

    create structure of naviagion

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator, createSwitchNavigator } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import AuthScreen from './screens/Auth';
+import Tab1MainScreen from './screens/Tab1MainPage';
+
+const AuthStackNavigation = createStackNavigator({
+  signIn: { screen: AuthScreen, navigationOptions: { header: null } },
+  signUp: { screen: Tab1MainScreen, navigationOptions: { title: '야호' } }
+}, {
+  initialRouteName: 'signIn',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  constructor(props) {
-    setTimeout(() => {
-      SplashScreen.hide();
-    }, 500)
-    super(props);
-    this.state = {
-      text: 'test'
-    };
-  }
+const MainScreenStackNavigation = createStackNavigator({
+  entry: { screen: AuthScreen, navigationOptions: { header: null } },
+}, {
+  initialRouteName: 'entry',
+})
 
-  componentDidMount() {
-  }
-  
-  componentDidAppear() {
-    this.setState({ text: 'power' });
-  }
+const FindScreenStackNavigation = createStackNavigator({
+  entry: { screen: AuthScreen, navigationOptions: { header: null } },
+}, {
+  initialRouteName: 'entry',
+})
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>come to React Native!</Text>
-      </View>
-      );
-  }
-}
+const WriteScreenStackNavigation = createStackNavigator({
+  entry: { screen: AuthScreen, navigationOptions: { header: null } },
+}, {
+  initialRouteName: 'entry',
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const MyInfoStackNavigation = createStackNavigator({
+  entry: { screen: AuthScreen, navigationOptions: { header: null } },
+}, {
+  initialRouteName: 'entry',
+})
+
+const MainBottomNavigation = createBottomTabNavigator({
+  main: { screen: AuthScreen, navigationOptions: { header: null } },
+  find: { screen: AuthScreen, navigationOptions: { header: null } },
+  write: { screen: AuthScreen, navigationOptions: { header: null } },
+  myInfo: { screen: AuthScreen, navigationOptions: { header: null } },
+}, {
+  initialRouteName: 'main',
+})
+
+export default createSwitchNavigator({
+  auth: AuthStackNavigation,
+  main: MainBottomNavigation
+})
