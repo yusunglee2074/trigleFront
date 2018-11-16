862a7a020f5b0e424dce720b79ec31f9888baabe
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2018-11-16 16:26:52 +0900

    write temp ui for address

--- src/screens/Tab4MyInfo/Address.js
+++ src/screens/Tab4MyInfo/Address.js
@@ -3,6 +3,8 @@ import {
   AsyncStorage, View, Text, Button, StyleSheet, SafeAreaView, TouchableOpacity, WebView
 } from 'react-native';
 
+import SplitTwoBar from './../Component/SplitTwoBar';
+
 class Address extends Component {
   constructor(props) {
     super(props);
@@ -18,8 +20,11 @@ class Address extends Component {
   }
 
   render () {
+    let tempList = [{name: '성유#12', address1: '경기도 수원시 우만동', address2: '세지로 420 301호'}, {name: '김철수', address1: '전라남도 목포시 용해동', address2: '동아아파트 301호'}]
     return (
       <SafeAreaView style={styles.container}>
+        <SplitTwoBar left={<Text>{tempList[0].name}</Text>} right={<Text>{tempList[0].address1}</Text>}></SplitTwoBar>
+        <SplitTwoBar left={<Text>{tempList[1].name}</Text>} right={<Text>{tempList[1].address1}</Text>}></SplitTwoBar>
       </SafeAreaView>
     );
   }
