7b3d51ecd247e5849fc8c0c3defcf835a02fb554
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2018-11-25 20:51:37 +0900

    add write page like gmail style

--- src/screens/Tab3Write/index.js
+++ src/screens/Tab3Write/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { 
-  AsyncStorage, View, Text, Button, StyleSheet, SafeAreaView, TouchableOpacity
+  AsyncStorage, View, Text, TextInput, StyleSheet, SafeAreaView, TouchableOpacity
 } from 'react-native';
+import { Icon } from 'react-native-elements'
 
 class Tab3Write extends Component {
   constructor(props) {
@@ -10,7 +11,36 @@ class Tab3Write extends Component {
     };
   }
 
+  static navigationOptions =  ({ navigation }) => {
+    return {
+      headerRight: (
+        <View style={{ flexDirection: 'row' }}>
+    <Icon
+      name='send-o'
+      type='font-awesome'
+      color='#f50'
+      onPress={navigation.getParam('add')} />
+    <Icon
+      name='mail-read'
+      type='octicon'
+      color='#f50'
+      onPress={navigation.getParam('add')} />
+    <Icon
+      name='paperclip'
+      type='font-awesome'
+      color='#f50'
+      onPress={navigation.getParam('add')} />
+        </View>
+        )
+    }
+  }
+
+  _add = () => {
+    alert('ㅇㅎ')
+  }
+
   componentDidMount() {
+    this.props.navigation.setParams({ add: this._add });
   }
   
   componentDidAppear() {
@@ -44,6 +74,44 @@ class Tab3Write extends Component {
   render () {
     return (
       <SafeAreaView style={styles.container}>
+
+        <View>
+          <View 
+            style={{flexDirection: 'row', alignItems:'center', paddingLeft: 10}}>
+            <Text
+              style={{flex: 0.2}}
+            >보낸사람</Text>
+            <TextInput
+              style={{flex: 0.8}}
+              onChangeText={(text) => this.setState({ text })}
+              placeholder="보낸사람"
+            ></TextInput>
+          </View>
+          <View 
+            style={{flexDirection: 'row', alignItems:'center', paddingLeft: 10}}>
+            <Text
+              style={{flex: 0.2}}
+            >받는사람</Text>
+            <TextInput
+              style={{flex: 0.8}}
+              onChangeText={(text) => this.setState({ text })}
+              placeholder="받는사람"
+            ></TextInput>
+          </View>
+        </View>
+        <View>
+          <View>
+            <TextInput
+              style={{ width: 200 }}
+              value={this.state.value}
+              onChangeText={text=>this.setState({value:text})}
+              multiline={true}
+              underlineColorAndroid='transparent'
+            />
+          </View>
+        </View>
+
+        { /*
         <ListItem
           onPress={() => this.navigate('letterPaper')}
           title="편지지"
@@ -73,6 +141,8 @@ class Tab3Write extends Component {
           <Text style={styles.price}>우편소모 3장</Text>
           <Button title="보내기" onPress={this.postMail}></Button>
         </View>
+        */ }
+
       </SafeAreaView>
     );
   }
@@ -93,8 +163,6 @@ class ListItem extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 30,
-    margin: 30,
   },
   content: {
     flex: 1,
