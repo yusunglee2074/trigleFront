b2549dcaf0bf7a95dc968831c0b78ee1956c8156
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2019-01-11 16:31:13 +0900

    api modulize

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.post['Accept'] = 'application/json'
+
+const apiUrl = Platform.OS === 'ios' ? "http://localhost:3000/graphql" : "http://10.0.2.2:3000/graphql";
+
+let post = (query) => {
+  return axios.post(apiUrl, {query}, {timeout: 1000});
+}
+
+let get = (query) => {
+  return axios.get(apiUrl, {query}, {timeout: 1000});
+}
+
+export default { post, get };
