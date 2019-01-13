ecb88e8549096ccef7d6b94935390274d8e91793
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2019-01-13 21:05:13 +0900

    setKeyword 스크린 완성
    유저에 키워드가 없을 시 유저 키워드로 보낸다.
    그 후 체크한 유저키워드들을 부모 스크린에 보내고 유저를 업데이트한다.

--- src/screens/Tab2Find/setKeyword.js
+++ src/screens/Tab2Find/setKeyword.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { 
-  FlatList, AsyncStorage, View, Text, Button, StyleSheet, SafeAreaView, TouchableOpacity, WebView
+  Alert, ScrollView, FlatList, AsyncStorage, View, Text, StyleSheet, SafeAreaView, TouchableOpacity, WebView
 } from 'react-native';
-import { Icon, SearchBar, Avatar } from 'react-native-elements';
+import { Icon, SearchBar, Avatar, Button } from 'react-native-elements';
 
 import api from './../../api';
 
@@ -10,62 +10,104 @@ class Address extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      defaultKeywords: [],
+      checkedKeywords: {},
     };
   }
 
   componentDidMount() {
-    this.props.navigation.setParams({ add: this._add });
+    const query = `
+    {
+      keywords(isDefaultKeyword: true) {
+        id
+        keyword
+      }
+    }
+    `
+    api.get(query)
+      .then(r => {
+        const keywords = Object.assign([], r.data.data.keywords);
+        this.setState({ defaultKeywords: keywords });
+      })
+      .catch(e => console.log(e))
   }
-  
-  componentDidAppear() {
-    this.setState({ text: 'power' });
+
+  clickKeywords = (item, index) => {
+    let defaultKeywords = Object.assign([], this.state.defaultKeywords);
+    defaultKeywords[index]['checked'] = defaultKeywords[index]['checked'] ? false : true;
+    let checkedKeywords = Object.assign({}, this.state.checkedKeywords);
+    checkedKeywords[index] = checkedKeywords[index] ? null : { keyword: item.keyword, id: item.id };
+    this.setState({ defaultKeywords: defaultKeywords, checkedKeywords })
+  }
+
+  saveKeyword = () => {
+    let keywords = this.state.checkedKeywords;
+    api.getStorageUser(AsyncStorage)
+      .then(user => {
+        let keywordCounter = 0;
+        let promiseList = [];
+        for (let key in keywords) {
+          if (!keywords[key]) continue;
+          keywordCounter++;
+          let query = `
+            mutation {
+              createUserKeyword(
+                userId: "${user.id}",
+                keywordId: "${keywords[key].id}"
+              ) {
+                userId {
+                  email
+                }
+                keywordId {
+                  keyword
+                }
+              }
+            }
+            `
+          promiseList.push(api.post(query));
+        }
+        if (!keywordCounter) {
+          Alert.alert("오류", "하나 이상의 키워드를 체크해주세요.");
+          throw "emptyKeywords";
+        }
+        return Promise.all(promiseList);
+      })
+      .then(r => {
+        this.props.navigation.state.params.setKeywords(keywords);
+        this.props.navigation.goBack(null)
+      })
+      .catch(e => console.log(e));
+
   }
 
   render () {
     return (
       <SafeAreaView style={styles.container}>
-        <FlatList
-          style={{ flex:1, marginHorizontal: 10 }}
-          data={this.state.data}
-          keyExtractor={(item, index) => item.id}
-          renderItem={({ item, index }) => {
-            return (
-                <TouchableOpacity
-                  onPress={() => this.navigate('profileDetail')}
-                  style={{ 
-                    borderWidth: 2,
-                    borderRadius: 2,
-                    borderColor: '#969696',
-                    height: 170,
-                    flex: 1,
-                    alignItems: 'center',
-                    paddingTop: 15,
-                    margin: 10,
-                    // ios
-                    backgroundColor: '#fffef9',
-                    shadowOffset: {width: 0, height: 13}, 
-                    shadowOpacity: 0.3,
-                    shadowRadius: 6,
-
-                    // android (Android +5.0)
-                    elevation: 3,
-                  }}>
-                  <Avatar
-                    size="large"
-                    source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
-                    containerStyle={{ borderWidth: 2, borderColor: 'tomato' }}
-                    onPress={() => console.log("Works!")}
-                    activeOpacity={0.7}
-                  />
-                  <Text>{item.nickname}</Text>
-                  <Text>#주부, #짜장면, #오버워치</Text>
-                  <View style={styles.divider}/>
-                  <Text>공통 키워드 4</Text>
-                </TouchableOpacity>
-            );
-          }}
-          numColumns= {2}
-        />
+        <ScrollView>
+          <FlatList
+            data = {this.state.defaultKeywords}
+            keyExtractor={(item, index) => index}
+            numColumns={4}
+            renderItem={({ item, index }) => (
+              <Button
+                key={index}
+                title={item.keyword}
+                titleStyle={{ color: 'black', fontWeight: 'normal', fontSize: 13 }}
+                onPress={() => this.clickKeywords(item, index)}
+                buttonStyle={{
+                  height: 40,
+                  backgroundColor: item.checked ? "red" : "transparent",
+                  borderColor: "rgba(92, 99,216, 1)",
+                  alignSelf: 'flex-start',
+                  borderWidth: 2,
+                  borderRadius: 5
+                }}
+                containerStyle={{ marginTop: 10, marginRight: 5 }}
+              />
+            )}
+          />
+        </ScrollView>
+        <Button title="저장" onPress={() => {this.saveKeyword()}}></Button>
       </SafeAreaView>
     );
   }
