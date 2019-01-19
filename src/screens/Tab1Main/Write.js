613db9169d0844cdf3eecddf8985c26eafd3e7ea
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2019-01-19 16:41:01 +0900

    add mainpage write screen

---
+++ src/screens/Tab1Main/Write.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { 
+  TouchableOpacity, FlatList, AsyncStorage, View, Text, TextInput, Button, Platform, StyleSheet, SafeAreaView
+} from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import api from './../../api';
+
+
+class WriteScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      contentText: '',
+    };
+  }
+
+  static navigationOptions =  ({ navigation }) => {
+    return {
+      headerRight: (
+        <TouchableOpacity
+          onPress={navigation.getParam('add')}
+        >
+          <View style={{marginRight: 18 }}>
+            <Text style={{ fontSize: 16, color: "rgb(31, 133, 250)" }}>게시</Text>
+          </View>
+        </TouchableOpacity>
+      )
+    }
+  }
+
+  _add = () => {
+    let st = this.state;
+    let query = `mutation {
+      createMail(
+        sender: "${st.user.nickname}",
+        senderId: "${st.user.id}",
+        content: "${st.contentText}",
+        isOffline: false,
+      ) {
+        id
+      }
+    }`;
+    api.post(query)
+      .then(r => {
+        if (r.status === 200) this.props.navigation.goBack(null);
+      })
+      .catch(e => console.log(e));
+  }
+
+  componentDidMount() {
+    api.getStorageUser(AsyncStorage)
+      .then(user => {
+        this.setState({user});
+      })
+    this.props.navigation.setParams({ add: this._add });
+  }
+  
+  navigate = (to) => {
+    switch(to) {
+      case 'mailDetail':
+        this.props.navigation.goBack(null);
+        break;
+    }
+  }
+
+  render () {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={{ flex: 1}}>
+          <View style={{ flex: 1, justifyContent: 'flex-start', marginLeft: 10, marginTop:10 }}>
+            <TextInput
+              style={{ fontSize: 16 }}
+              value={this.state.contentText}
+              onChangeText={contentText =>this.setState({ contentText })}
+              multiline={true}
+              underlineColorAndroid='transparent'
+              placeholder='내용 작성'
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#33373B',
+  },
+});
+
+export default WriteScreen;
