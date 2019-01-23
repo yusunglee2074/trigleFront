c32aed0fd9912b9c1ff73ba41abff920630ec98e
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2019-01-23 15:12:26 +0900

    공지사항 부분 자주묻는질문 스크린 복사해옴

--- src/screens/Tab4MyInfo/Statistics.js
+++ src/screens/Tab4MyInfo/Statistics.js
@@ -2,26 +2,66 @@ import React, { Component } from 'react';
 import { 
   AsyncStorage, View, Text, Button, StyleSheet, SafeAreaView, TouchableOpacity, WebView
 } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import api from './../../api';
 
 class Statistics extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      env: '',
+      isLoading: true,
     };
   }
 
   componentDidMount() {
+    const query = `{
+      extraEnvs(
+        type: "notice"
+      ) {
+        id
+        title
+        content
+      }
+    }`;
+    api.get(query).then(r => {
+      this.setState({ env: r.data.data.extraEnvs, isLoading: false });
+    });
   }
   
   componentDidAppear() {
-    this.setState({ text: 'power' });
+  }
+
+  togle = (i) => {
+    let envs = this.state.env;
+    envs[i].checked = !Boolean(envs[i].checked);
+    this.setState({ env: envs })
   }
 
   render () {
-    return (
-      <SafeAreaView style={styles.container}>
-      </SafeAreaView>
-    );
+    if (this.state.isLoading) {
+      return (<Text>로딩중</Text>);
+    } else {
+      return (
+        <SafeAreaView style={styles.container}>
+          <View>
+            {
+              this.state.env.map((item, i) => (
+                <TouchableOpacity 
+                  onPress={() => this.togle(i)}
+                  key={i}>
+                  <ListItem
+                    rightIcon={{ name: 'arrow-right', type: 'simple-line-icon' }}
+                    title={item.title}
+                  />
+                  { item.checked ? <Text>{ item.content}</Text> : void 0 }
+                </TouchableOpacity>
+              ))
+            }
+          </View>
+        </SafeAreaView>
+      );
+    }
   }
 }
 
